a43a8d57faa331fe48ed1dfcafcaeeccbe59651b
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-12-18 11:19:01 +0000

    Finished results

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ document.getElementById('submit')
         //     const panel = new Mesh(
         //         new BoxGeometry(4.5, 0.02, 4.5),
         //         new MeshStandardMaterial({
-        //             color,
+        //             color: '#CCCCCC',
         //             roughness,
         //             metalness,
         //         })
@@ -85,62 +85,89 @@ document.getElementById('submit')
         //     panel.translateZ(-4.5 / 2 - 0.02);
         //     panel.rotateZ(thetaOffset + (-i) / sides * Math.PI * 2)
         //     scene.add(panel);
-        // }
-
-        // Arrange the mirrors.
-        // const angles = [-Math.PI / 10, Math.PI / 10]
-        // for (let i = 0; i < 2; i++) {
-        //     const panel = new Mesh(
-        //         new BoxGeometry(4.5, 0.02, 4.5),
+        //
+        //     const outerPanel = new Mesh(
+        //         new BoxGeometry(4.5, 0.002, 4.5),
         //         new MeshStandardMaterial({
-        //             color,
-        //             roughness,
-        //             metalness,
+        //             color: '#FF0000',
+        //             roughness: 0.4,
+        //             metalness: 0.5
         //         })
         //     );
-        //     const distance = 1.25;
-        //     const thetaOffset = 0;
-        //     panel.translateY(-0.5)
-        //     panel.translateX(Math.sin(thetaOffset + angles[i]) * 4.58 / 2)
-        //     panel.translateZ(-4.5 / 2 - 0.02);
-        //     panel.rotateZ(thetaOffset + angles[i] + Math.PI / 2)
-        //     scene.add(panel);
+        //     outerPanel.translateY(Math.cos(thetaOffset + i / sides * Math.PI) * distance*1.01)
+        //     outerPanel.translateX(Math.sin(thetaOffset + i / sides * Math.PI) * distance*1.01)
+        //     outerPanel.translateZ(-4.5 / 2 - 0.02);
+        //     outerPanel.rotateZ(thetaOffset + (-i) / sides * Math.PI * 2)
+        //     scene.add(outerPanel);
         // }
 
         // Arrange the mirrors.
-        for (let i = 0; i < 3; i++) {
-            const sideLength = 3.4;
-            const height = 4.5
+        const angles = [-Math.PI / 10, Math.PI / 10]
+        for (let i = 0; i < 2; i++) {
             const panel = new Mesh(
-                new BoxGeometry(sideLength, 0.02, height),
+                new BoxGeometry(4.5, 0.02, 4.5),
                 new MeshStandardMaterial({
                     color,
                     roughness,
                     metalness,
                 })
             );
-            const distance = sideLength * (1.303 / 4.5);
-            const thetaOffset = 0.2;
-            panel.translateY(Math.cos(thetaOffset + i / sides * Math.PI * 2) * distance)
-            panel.translateX(Math.sin(thetaOffset + i / sides * Math.PI * 2) * distance)
-            panel.translateZ(-height / 2 - 0.02);
-            panel.rotateZ(-thetaOffset + (-i) / sides * Math.PI * 2)
+            const thetaOffset = 0;
+            panel.translateY(-0.5)
+            panel.translateX(Math.sin(thetaOffset + angles[i]) * 4.54 / 2)
+            panel.translateZ(-4.5 / 2 - 0.02);
+            panel.rotateZ(thetaOffset + angles[i] + Math.PI / 2)
             scene.add(panel);
 
             const outerPanel = new Mesh(
-                new BoxGeometry(sideLength + 0.02, 0.002, height),
+                new BoxGeometry(4.5 + 0.02, 0.002, 4.5),
                 new MeshStandardMaterial({
                     color: '#FF0000',
                     roughness: 0.4,
                     metalness: 0.5
                 })
             );
-            outerPanel.translateY(Math.cos(thetaOffset + i / sides * Math.PI * 2) * distance*1.01)
-            outerPanel.translateX(Math.sin(thetaOffset + i / sides * Math.PI * 2) * distance*1.01)
-            outerPanel.translateZ(-height / 2 - 0.02);
-            outerPanel.rotateZ(-thetaOffset + (-i) / sides * Math.PI * 2)
+            outerPanel.translateY((-0.5) * 0.99)
+            outerPanel.translateX(Math.sin(thetaOffset + angles[i]) * 4.54 / 2 * 1.02)
+            outerPanel.translateZ(-4.5 / 2 - 0.02);
+            outerPanel.rotateZ(thetaOffset + angles[i] + Math.PI / 2)
             scene.add(outerPanel);
         }
+
+        // Arrange the mirrors.
+        // for (let i = 0; i < 3; i++) {
+        //     const sideLength = 3.4;
+        //     const height = 4.5
+        //     const panel = new Mesh(
+        //         new BoxGeometry(sideLength, 0.02, height),
+        //         new MeshStandardMaterial({
+        //             color,
+        //             roughness,
+        //             metalness,
+        //         })
+        //     );
+        //     const distance = sideLength * (1.303 / 4.5);
+        //     const thetaOffset = 0.2;
+        //     panel.translateY(Math.cos(thetaOffset + i / sides * Math.PI * 2) * distance)
+        //     panel.translateX(Math.sin(thetaOffset + i / sides * Math.PI * 2) * distance)
+        //     panel.translateZ(-height / 2 - 0.02);
+        //     panel.rotateZ(-thetaOffset + (-i) / sides * Math.PI * 2)
+        //     scene.add(panel);
+        //
+        //     const outerPanel = new Mesh(
+        //         new BoxGeometry(sideLength + 0.02, 0.002, height),
+        //         new MeshStandardMaterial({
+        //             color: '#FF0000',
+        //             roughness: 0.4,
+        //             metalness: 0.5
+        //         })
+        //     );
+        //     outerPanel.translateY(Math.cos(thetaOffset + i / sides * Math.PI * 2) * distance*1.01)
+        //     outerPanel.translateX(Math.sin(thetaOffset + i / sides * Math.PI * 2) * distance*1.01)
+        //     outerPanel.translateZ(-height / 2 - 0.02);
+        //     outerPanel.rotateZ(-thetaOffset + (-i) / sides * Math.PI * 2)
+        //     scene.add(outerPanel);
+        // }
 
         function startRenderer(scene) {
             // set the environment map
@@ -156,6 +183,9 @@ document.getElementById('submit')
 
             camera.position.set(0, -6, -length / 1.2);
             camera.lookAt(0, 2, 0);
+
+            // camera.position.set(0, 0, -length / 1);
+            // camera.lookAt(0, 0, 0);
 
             renderer.toneMapping = LinearToneMapping;
 
@@ -179,11 +209,11 @@ document.getElementById('submit')
             animate();
         }
 
-        const light= false
+        const light= true
         const backdrop = new Mesh(
             new BoxGeometry(1000, 0.01, 1000),
             new MeshStandardMaterial({
-                color: light ? "#F8F8F8" : "#EEEEEE",
+                color: light ? "#EEEEEE" : "#212121",
             })
         );
         backdrop.rotateX(-Math.PI / 2);
